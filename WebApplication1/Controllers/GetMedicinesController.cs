81bdbfc29241698a207acf87e2bbc93ba6f5289a
MyEdit <chekushka.borodin@gmail.com>
2024-03-12 12:46:57 +0300

    Добавлен метод для отправки структуры или класса

--- WebApplication1/Controllers/GetMedicinesController.cs
+++ WebApplication1/Controllers/GetMedicinesController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace WebApplication15.Controllers
 {
@@ -6,18 +9,41 @@ namespace WebApplication15.Controllers
     [Route("api/[controller]")]
     public class GetMedicinesController : ControllerBase
     {
+
         [HttpGet]
         public ActionResult<object> Get()
         {
             List<string[]> data =
             [
                 ["Парацетамол", "Пушкинский", "789"],
-                [ "Цитрамон", "Пушкинский", "986" ],
-                [ "Нош-па", "Алексеевский", "874" ],
-                [ "Ибупрофен", "Ивановский", "578" ]
+                ["Цитрамон", "Пушкинский", "986"],
+                ["Нош-па", "Алексеевский", "874"],
+                ["Ибупрофен", "Ивановский", "578"]
             ];
 
-            return new {data};
+            return new { data };
         }
+
+        //return struct or class
+        /*
+            struct Player
+            {
+                public string name { get; set; }
+                public int age { get; set; }
+                public DateTime dateBirthday { get; set; }
+            }
+            [HttpGet]
+            public ActionResult<object> Get()
+            {
+                List<Player> players =
+                [
+                    new Player() { name = "Alex", age = 5, dateBirthday = DateTime.Now.Date },
+                    new Player() { name = "Oleg", age = 35, dateBirthday = DateTime.Now.Date },
+                    new Player() { name = "Ruslan", age = 47, dateBirthday = DateTime.Now.Date }
+                ];
+
+                return new { players };
+            }
+        */
     }
 }
